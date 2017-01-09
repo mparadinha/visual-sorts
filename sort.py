4989aa2d628872b09fe306a0b8d07c8d6486bc92
Miguel Paradinha <miguel.p.paradinha@gmail.com>
2017-01-09 01:56:09 +0000

    bars scale with array size and so does the time for final animation

--- sort.py
+++ sort.py
@@ -65,38 +65,40 @@ def main(args):
                 return
 
 def show_array(screen, array, highlights):
-    x = 1
-    bar_size = round((700 / (1.4 * len(array) - 1)))
-    space = round(0.4 * bar_size)
-    
+    bar_size = (700 / len(array)) / 1.4
+    space = 0.4 * bar_size
+
+    x = space // 2
     for i, val in enumerate(array):
         y = round((500 * val) / len(array))
-        print(i, val, x, y)
+
         # green bars for current bars. white for all others
         color = (0, 255, 0) if i in highlights else (255, 255, 255)
 
-        pygame.draw.rect(screen, color, pygame.Rect(x, 501 - y, bar_size, y))
+        pygame.draw.rect(screen, color, pygame.Rect(round(x), 501 - y, round(bar_size), y))
         x += bar_size + space
 
     pygame.display.update()
-    
+ 
 def draw_completed_array(screen, array):
-    x = 1
-    bar_size = round((700 / (1.4 * len(array) - 1)))
-    space = round(0.4 * bar_size)
-    
+    bar_size = (700 / len(array)) / 1.4
+    space = 0.4 * bar_size
+
+    # we have 500 ms for the whole array
+    wait_time = 500 // len(array)
+
     # this is just because the switching looks ugly otherwise
     # because the last switched indexes are still green
     show_array(screen, array, ())
-    
-    for i, val in enumerate(array):
-        y = (500 * val) // len(array)
-        color = (0, 255, 0)
 
-        pygame.draw.rect(screen, color, pygame.Rect(x, 501 - y, bar_size, y))
+    x = space // 2 
+    for i, val in enumerate(array):
+        y = round((500 * val) / len(array))
+
+        pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(round(x), 501 - y, round(bar_size), y))
         x += bar_size + space
         
-        pygame.time.wait(5)
+        pygame.time.wait(wait_time)
         pygame.display.update()
 
 def bubble(array):
